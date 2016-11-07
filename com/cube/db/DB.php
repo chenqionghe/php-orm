912f0eb74f78bf80b217fba7527984d75fa5890e
linyang <lin2798003@sina.com>
2016-11-07 14:21:29 +0800

    change the unique insert

--- com/cube/db/DB.php
+++ com/cube/db/DB.php
@@ -100,8 +100,8 @@ final class DB
     {
         if (!empty(self::$pdo)) {
             try {
+                Log::mysql($sql . ' task: ' . $task ? 'true' : 'false');
                 if ($task == true) {
-                    self::$pdo->query('SET AUTOCOMMIT=0');
                     self::$pdo->query('BEGIN');
                 }
 
@@ -113,10 +113,10 @@ final class DB
                 } else {
                     if ($task == true) self::$pdo->query('COMMIT');
                 }
-                Log::mysql($sql . ' => sql back line ' . $lineNum . ' task: ' . $task ? 'true' : 'false');
+                Log::mysql('=> sql back line ' . $lineNum);
                 return $lineNum;
             } catch (\PDOException $e) {
-                Log::mysql($sql . ' => Error ' . $e->getTraceAsString());
+                Log::mysql('=> Error ' . $e->getTraceAsString());
                 return -1;
             }
         }
@@ -479,7 +479,7 @@ class DBModel
 
             if (!empty($this->_where)) {
                 $unique_key = explode('=', $this->_where)[0];
-                $sql .= ' SELECT (' . join(',', $values) . ') FROM DUAL WHERE NOT EXISTS(SELECT ';
+                $sql .= ' SELECT ' . join(',', $values) . ' FROM DUAL WHERE NOT EXISTS(SELECT ';
                 $sql .= $unique_key . ' FROM ' . $this->_table_name . ' WHERE ' . $this->_where . ')';
             } else {
                 $sql .= ' VALUES (' . join(',', $values) . ')';
